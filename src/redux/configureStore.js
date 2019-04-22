63e0ee503a2bf3454da2b608668ffb56cd7bc300
Rupali Sinha <rupalisinha23@gmail.com>
2019-04-22 22:07:35 +0200

    Combine Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 // function to create the redux store
 export const ConfigureStore = () => {
-    const store = createStore(Reducer, initialState);
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments:Comments,
+            promotions:Promotions,
+            leaders:Leaders
+
+        })
+    );
     return store;
 }
