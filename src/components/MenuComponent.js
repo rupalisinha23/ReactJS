adccc7d18618da1ae7b3c058b24aeab16ee20cdc
Rupali Sinha <rupalisinha23@gmail.com>
2019-05-07 11:49:24 +0200

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 function RenderMenuItem({dish, onClick}){
@@ -20,31 +21,46 @@ function RenderMenuItem({dish, onClick}){
 
 const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div key={dish.id} className='col-12 col-md-5 m-1'>
                    <RenderMenuItem dish = {dish} /> 
                 </div>
             );
         });
-        return (
-            <div className='container'>
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem> <Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Menu</h3>
-                        <hr />
+        if (props.dishes.isLoading){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
                     </div>
                 </div>
+            );
+        }
+        else if (props.dishes.errMess){
+            return (
+                <h4>{props.dishes.errMess}</h4>
+            );
+        }
+        else
+            return (
+                <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem> <Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>
+                    </div>
 
-                <div className='row'>
-                    {menu}
+                    <div className='row'>
+                        {menu}
+                    </div>
                 </div>
-            </div>
-        );
+            );
     }
 
 
